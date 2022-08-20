f84309dbb305bc6aba4baf4701b6944aeb53ad6e
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-08-20 15:02:59 +0600

    inputting and printing array by using while loop

---
+++ input loop.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,k;
+    cin>>n;
+    int a[k=n];
+    int i=0
+    while(k--)
+    {
+        cin>>a[k];
+    }
+
+    while(i<k)
+    {
+        cout<<a[i]<<" ";
+        i++;
+    }
+//    while(n--)
+//    {
+//        cout<<a[n]<< " ";
+//    }
+}
