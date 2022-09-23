fe1d8d768f5b39d52e91e23612bdc4e3254f6956
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-23 23:47:38 +0600

    The initial value of all index is zero for global array

---
+++ global array.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int arr[10000];
+int main()
+{
+    cout<<arr[0]<<endl;
+    cout<<arr[100]<<endl;
+    cout<<arr[1000]<<endl;
+    cout<<arr[1999]<<endl;
+
+}
