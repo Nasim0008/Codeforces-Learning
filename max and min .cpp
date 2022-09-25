609fc7505d3d9f7ef379f1926a0fbf23baf84425
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-25 14:10:08 +0600

    find the max and min value from the multiple variable

---
+++ max and min .cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    int mx = max({a,b,c,d,e});
+    int mn = min({a,b,c,d,e});
+    cout<<mx<<endl;
+    cout<<mn<<endl;
+}
+
