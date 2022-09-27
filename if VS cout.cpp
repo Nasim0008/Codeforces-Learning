1cff2161012b01a1ece4d2f4a875839273c0fe60
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-27 20:43:05 +0600

    if VS cout statement

---
+++ if VS cout.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int a,b;
+        cin>>a>>b;
+        cout<<(a>b)<<endl;// print 1 if condition is true or print 0 if condition is false
+        cout<<(a<b)<<endl;
+}
+
