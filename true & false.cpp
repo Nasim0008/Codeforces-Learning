a5156d1217e9019583abe42a3c21f65077e87d36
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-29 16:20:56 +0600

    conversion true and false

---
+++ true & false.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    bool nasim=true;
+    if(!nasim)
+    {
+        cout<< "Nasim"<<endl;
+    }
+    else
+    {
+        cout<< "Google"<<endl;
+    }
+}
