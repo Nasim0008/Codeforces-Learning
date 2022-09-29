09dd40d15171220868235aafa117a22099af7bc6
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-29 16:13:20 +0600

    convert zero to non zero and non-zero to zero

---
+++ zero to non zero.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(!n)// it convert non-zero to zero or zero to non-zero
+    {
+        cout<< "Nasim"<<endl;
+    }
+    else
+    {
+        cout<< "Google"<<endl;
+    }
+}
+
