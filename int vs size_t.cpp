31e2d19075d7a751f39a363f4d5177be1be9d76c
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-30 15:37:17 +0600

    int Vs size_t Vs int64_t

---
+++ int vs size_t.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=123456789987654321;
+    size_t a=123456789987654321;
+    int64_t b=123456789987654321;
+    cout<<n<<endl<<a<<endl<<b<<endl;
+}
