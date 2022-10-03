d5d8c515d79d30f0cce018a9a5112aff9f9bc8fe
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-10-03 17:16:34 +0600

    round,floor and ceil value

---
+++ round , floor and ceiling.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<< "Round value: ( 0.0 to 0.4):"<<endl;
+    cout<< round(20.0)<<endl;
+    cout<< round(20.1)<<endl;
+    cout<< round(20.2)<<endl;
+    cout<< round(20.3)<<endl;
+    cout<< round(20.4)<<endl;
+
+    cout<< "round value: (0.5 to 0.9): "<<endl;
+    cout<< round(20.5)<<endl;
+    cout<< round(20.6)<<endl;
+    cout<< round(20.7)<<endl;
+    cout<< round(20.8)<<endl;
+    cout<< round(20.9)<<endl;
+    cout<< "floor value : "<<endl;
+    cout<< floor(20.1)<<endl;
+    cout<< floor(20.2)<<endl;
+    cout<< floor(20.3)<<endl;
+    cout<< floor(20.4)<<endl;
+    cout<< floor(20.5)<<endl;
+    cout<< floor(20.6)<<endl;
+    cout<< floor(20.7)<<endl;
+    cout<< floor(20.8)<<endl;
+    cout<< floor(20.9)<<endl;
+
+    cout<< "Ceiling value: "<<endl;
+
+    cout<< ceil(20.1)<<endl;
+    cout<< ceil(20.2)<<endl;
+    cout<< ceil(20.3)<<endl;
+    cout<< ceil(20.4)<<endl;
+    cout<< ceil(20.5)<<endl;
+    cout<< ceil(20.6)<<endl;
+    cout<< ceil(20.7)<<endl;
+    cout<< ceil(20.8)<<endl;
+    cout<< ceil(20.9)<<endl;
+
+
+}
+
