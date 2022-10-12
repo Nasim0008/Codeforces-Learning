ca69d2d7563d69119c08f76f7167ed73be67a963
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-10-12 15:46:01 +0600

    long long define above the main function

---
+++ define long long.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int n;
+   cin>>n;
+   if(n%2)
+   {
+       cout<<n<< " is a odd number"<<endl;
+   }
+   else
+   {
+       cout<<n<< " is a even number"<<endl;
+   }
+
+}
