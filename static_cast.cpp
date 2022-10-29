7d26e17b175a2dfd6ebb7910f283f592cecf0958
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-10-29 13:04:36 +0600

    type casting by using static_cast

---
+++ static_cast.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a = 1.22222;
+    cout<< static_cast<int>(a)<<endl;
+    int n =7;
+    cout<< static_cast<char>(n)<<endl;
+}
+
+
+
+
