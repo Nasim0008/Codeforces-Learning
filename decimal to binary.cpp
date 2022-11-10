559ce4e9afbd080e28a3a5547d3ad6a3cf80f36e
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-11-10 23:27:05 +0600

    Decimal to binary conversion

--- decimal to binary.cpp
+++ decimal to binary.cpp
@@ -6,11 +6,12 @@ int main()
    int n;
    cin>>n;
    vector<int>v;
-   while(n!=0)
+   while(n>0)
    {
-       n/=2;
+        n/=2;
        int rem=n%2;
        v.push_back(rem);
+
    }
    for(int i=0;i<v.size();i++)
    {
