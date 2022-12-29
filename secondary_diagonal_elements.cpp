c2c89c3097e310462efe6317af9f89a01ffab7fa
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-12-29 18:18:02 +0600

    secondary diagonal elements of a matrix

---
+++ secondary_diagonal_elements.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    char s[n][n];
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cin>>s[i][j];
+        }
+    }
+    string a="";
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(i==(n-j+1))
+            {
+                a+=s[i][j];
+            }
+        }
+    }
+    cout<<a<<endl;
+}
