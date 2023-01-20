8404e7ba657bad281dc7bae4e63d733e2798aecc
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-01-20 09:49:57 +0600

    find the prefix sum by using partial sum function

--- Prefix Sum/prefix_sum.cpp
+++ Prefix Sum/prefix_sum.cpp
@@ -1,1 +1,19 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n+3];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int a[n+3];// prefix_sum array
+    partial_sum(arr,arr+n,a);
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<< " "<<arr[i]<<endl;
+    }
+    
+}
