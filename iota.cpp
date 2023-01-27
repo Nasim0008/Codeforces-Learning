c31b3010ae58caee0a900ff18cda926db34c983e
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-01-27 15:11:07 +0600

    set the consicutive natural number in the array by using iota fucntion

---
+++ iota.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    iota(a,a+n,10);
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<endl;
+    }
+}
