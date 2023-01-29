8fa3e5f4f5043c90779cf3507937cc68181fe3fa
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-01-29 15:30:28 +0600

    Theory for the Lower bound

---
+++ upper and lower bound/lower_bound.cpp
@@ -0,0 +1,27 @@
+/*
+There are 4 case happens in lower bound:
+
+1. If the searching value is present and it is one times appeares:
+=> It return the index of that value.
+
+2. If the searching value is not present and it is not greater than the last value:
+=> It return the index of the next value as the sequence of ascending order
+=> stricly  greater than value's index of the sorting array
+
+3. If the searching value is not present and it is greater than the last value:
+=> return Not found
+
+4. If the searching value is present and it is multiple times appeares:
+=> it return the first appeares value's index
+
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   cout<< "Read the comment carefully"<<endl;
+    
+}
