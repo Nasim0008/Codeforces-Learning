5cc7cb678291a96e6d2f3c2c1f18711907910ec3
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-10-15 13:18:22 +0600

    Total number of subarray

---
+++ array/total_number_of_subarray.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long n;
+    cin>>n;
+    int total_number_of_subarray = (n*(n+1)/2);
+    cout<< total_number_of_subarray<<endl;
+}
